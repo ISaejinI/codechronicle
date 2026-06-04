63e5f97063a15c314f6e641ec7aebd6fdff03284
Lou-Anne <saejinmx@gmail.com>
2026-06-04 14:58:54 +0200

    Ajout d'un workflow pour convertir les articles Markdown en HTML et mise à jour de l'action Discord pour inclure le contenu généré.

---
+++ scripts/convert-to-html.js
@@ -0,0 +1,9 @@
+import markdown, { getCodeString } from '@wcj/markdown-to-html';
+import fs from 'fs';
+
+const content = process.env.CONTENT;
+const fileName = process.env.FILE_NAME;
+
+const htmlContent = markdown(content);
+
+fs.writeFileSync(`public/blog/${fileName}.html`, htmlContent);
